2d8332eef1c3aa7934092777c53be9bbb7006d8e
VladimirZhdanov <consthomespb@gmail.com>
2018-08-04 13:13:48 +0300

    add javaDocs to ConverterTest.java

--- chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
+++ chapter_001/src/test/java/ru/job4j/converter/ConverterTest.java
@@ -4,7 +4,19 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test.
+ *
+ * @author Vladimir Zhdanov (mailto:constHomeSpb@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
 public class ConverterTest {
+
+    /**
+     * Test rubleToEuro().
+     */
     @Test
     public void whenRateOfEuroToEuroThen1() {
         Converter converter = new Converter();
@@ -12,6 +24,9 @@ public class ConverterTest {
         assertThat(result, is(1d));
     }
 
+    /**
+     * Test rubleToDollar().
+     */
     @Test
     public void whenRateOfDollarToDollarThen1() {
         Converter converter = new Converter();
