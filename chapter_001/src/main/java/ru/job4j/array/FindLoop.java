7af85363dfbf285ab36290a04d28688b51745350
VladimirZhdanov <consthomespb@gmail.com>
2018-08-18 12:05:12 +0300

    6.0. Заполнить массив степенями чисел. [#33488]

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * FindLoop
+ *
+ * @author Vladimir Zhdanov (mailto:constHomeSpb@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class FindLoop {
+
+    /**
+     * Board of Strings.
+     * @param data - array.
+     * @param el - nummber.
+     * @return index of el or -1.
+     */
+    public int indexOf(int[] data, int el) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                rst = i;
+                break;
+            }
+        }
+        return rst;
+    }
+}
