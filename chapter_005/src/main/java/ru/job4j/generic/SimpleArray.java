6655c0458e8b63c4258e58bfa277c7ff750c5af3
VladimirZhdanov <consthomespb@gmail.com>
2019-02-18 22:09:05 +0300

    5.2.1. Реализовать SimpleArray<T>

--- chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
+++ chapter_005/src/main/java/ru/job4j/generic/SimpleArray.java
@@ -1,28 +1,118 @@
 package ru.job4j.generic;
 
-import java.util.Iterator;
+import java.util.*;
 
+/**
+ * SimpleArray
+ *
+ * @author Vladimir Zhdanov (mailto:constHomeSpb@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class SimpleArray<T> implements Iterable<T> {
-    private Object[] objects = new Object[10];
+    private T[] objects;
+    private int currentSize;
+    private int i = 0;
 
-/*    add(T model) {
-
+    public SimpleArray(T[] objects) {
+        this.objects = objects;
+        this.currentSize = objects.length;
     }
 
-    set(int index, T model) {
-
+    public boolean add(T model) {
+        boolean result = false;
+        for (int i = 0; i < currentSize; i++) {
+            if (null == objects[i]) {
+                objects[i] = model;
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
-    remove(int index) {
-
+    public void set(int index, T model) {
+        Objects.checkIndex(index, currentSize);
+        if (currentSize > index) {
+            this.objects[index] = model;
+        }
     }
 
-    get(int index) {
+    public void remove(int index) {
+        Objects.checkIndex(index, currentSize);
+        final int newSize = currentSize - 1;
+        if (newSize > index) {
+            System.arraycopy(objects, index + 1, objects, index, newSize - index);
+        }
+        objects[currentSize - 1] = null;
+    }
 
+    public Object get(int index) {
+        Objects.checkIndex(index, currentSize);
+        return objects[index];
+    }
+
+    /*public static void main(String[] args) {
+        SimpleArray<String> sa = new SimpleArray<String>(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"});
+        sa.remove(9);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(sa);
+        }
     }*/
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        Iterator<T> it = new Iterator<T>() {
+
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < currentSize && objects[currentIndex] != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                } else {
+                    return objects[currentIndex++];
+                }
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        };
+        return it;
+    }
+
+    @Override
+    public String toString() {
+        return "SimpleArray{"
+                + "objects=" + Arrays.toString(objects)
+                + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        SimpleArray<?> that = (SimpleArray<?>) o;
+        return currentSize == that.currentSize
+                && i == that.i
+                && Arrays.equals(objects, that.objects);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(currentSize, i);
+        result = 31 * result + Arrays.hashCode(objects);
+        return result;
     }
 }
---
+++ chapter_005/src/test/java/ru/job4j/generic/SimpleArrayTest.java
@@ -0,0 +1,81 @@
+package ru.job4j.generic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * SimpleArrayTest
+ *
+ * @author Vladimir Zhdanov (mailto:constHomeSpb@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class SimpleArrayTest {
+    private SimpleArray<String> sa;
+    private Iterator<String> iterator;
+
+    @Before
+    public void setUp() {
+        sa = new SimpleArray<>(new String[] {"1", "2", "3", "4", "5"});
+        iterator = sa.iterator();
+    }
+
+    @Test
+    public void shouldSequentiallyReturnItems() {
+        assertThat(iterator.next(), is("1"));
+        assertThat(iterator.next(), is("2"));
+        assertThat(iterator.next(), is("3"));
+        assertThat(iterator.next(), is("4"));
+        assertThat(iterator.next(), is("5"));
+        //iterator.next();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void shouldReturnItemsSequentially() {
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("1"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("2"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("3"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("4"));
+        assertThat(iterator.hasNext(), is(true));
+        assertThat(iterator.next(), is("5"));
+        assertThat(iterator.hasNext(), is(false));
+        iterator.next();
+    }
+
+    @Test
+    public void  shouldReturnFalseIfNoAnyItems() {
+        sa = new SimpleArray<>(new String[]{});
+        iterator = sa.iterator();
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    @Test
+    public void  whenRemoveAndAddThen() {
+        sa.remove(0);
+        sa.add("new");
+        SimpleArray expected = new SimpleArray<>(new String[] {"2", "3", "4", "5", "new"});
+        assertThat(sa, is(expected));
+    }
+
+    @Test
+    public void  whenSetThen() {
+        sa.set(0, "new");
+        SimpleArray expected = new SimpleArray<>(new String[] {"new", "2", "3", "4", "5"});
+        assertThat(sa, is(expected));
+    }
+
+    @Test
+    public void  whenGetThenShow() {
+        assertThat(sa.get(0), is("1"));
+    }
+}
