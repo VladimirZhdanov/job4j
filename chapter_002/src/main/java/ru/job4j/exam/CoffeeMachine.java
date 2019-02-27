7a1cfcda4cce2537ee997c2fd96f39895b0581fb
VladimirZhdanov <consthomespb@gmail.com>
2019-02-27 19:55:29 +0300

    Кофемашина. 2

--- chapter_002/src/main/java/ru/job4j/exam/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/exam/CoffeeMachine.java
@@ -1,36 +1,76 @@
 package ru.job4j.exam;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 public class CoffeeMachine {
+    private int ten = 10;
+    private int countOfTen;
 
-    public int[] changes(int value, int price) throws InsufficientFundsException {
+    private int five = 5;
+    private int countOfFive;
+
+    private int two = 2;
+    private int countOfTwo;
+
+    private int one = 1;
+    private int countOfOne;
+    int[] buffer;
+
+    public CoffeeMachine(int countOfTen, int countOfFive, int countOfTwo, int countOfOne) {
+        this.countOfTen = countOfTen;
+        this.countOfFive = countOfFive;
+        this.countOfTwo = countOfTwo;
+        this.countOfOne = countOfOne;
+    }
+
+
+    private boolean check(int value, int price) throws InsufficientFundsException {
+        boolean result = false;
         int change = value - price;
         if (change < 0) {
             throw new InsufficientFundsException("Insufficient funds, Could you please put additional money?");
         }
         ArrayList<Integer> array = new ArrayList<>();
-        while (change >= 10) {
-            change -= 10;
-            array.add(10);
+        while (change >= ten && countOfTen > 0) {
+            change -= ten;
+            array.add(ten);
+            countOfTen--;
         }
-        while (change >= 5) {
-            change -= 5;
-            array.add(5);
+        while (change >= five && countOfFive > 0) {
+            change -= five;
+            array.add(five);
+            countOfFive--;
         }
-        while (change >= 2) {
-            change -= 2;
-            array.add(2);
+        while (change >= two && countOfTwo > 0) {
+            change -= two;
+            array.add(two);
+            countOfTwo--;
         }
-        while (change > 0) {
-            change -= 1;
-            array.add(1);
+        while (change > 0 && countOfOne > 0) {
+            change -= one;
+            array.add(one);
+            countOfOne--;
         }
-        return array.stream().mapToInt(x -> x).toArray();
+
+        int sum = array.stream().mapToInt(x -> x).sum();
+        if (sum == value - price) {
+            buffer = array.stream().mapToInt(x -> x).toArray();
+            result = true;
+        }
+        return result;
+    }
+
+    public int[] changes(int value, int price) throws InsufficientFundsException {
+        if (check(value, price)) {
+            return buffer;
+        } else {
+            return new int[] {value};
+        }
     }
     /*public static void main(String[] args) {
-        CoffeeMachine cf = new CoffeeMachine();
-        int[] sa = cf.changes(100, 101);
+        CoffeeMachine cf = new CoffeeMachine(10, 1, 5, 2, 2, 5, 1, 3);
+        int[] sa = cf.changes(100, 70);
         for (int i = 0; i < sa.length; i++) {
             System.out.println(sa[i]);
         }
--- chapter_002/src/test/java/ru/job4j/exam/CoffeeMachineTest.java
+++ chapter_002/src/test/java/ru/job4j/exam/CoffeeMachineTest.java
@@ -17,7 +17,7 @@ public class CoffeeMachineTest {
 
     @Test
     public void  whenPrise100andValue82Return10and5and2and1() {
-        CoffeeMachine cf = new CoffeeMachine();
+        CoffeeMachine cf = new CoffeeMachine(10, 10, 5, 5);
         int[] result = cf.changes(100, 82);
         int[] expected = new int[] {10, 5, 2, 1};
         assertThat(result, is(expected));
@@ -25,7 +25,15 @@ public class CoffeeMachineTest {
 
     @Test (expected = InsufficientFundsException.class)
     public void whenPriceGrater() {
-        CoffeeMachine cf = new CoffeeMachine();
+        CoffeeMachine cf = new CoffeeMachine(10, 10, 5, 5);
         int[] result = cf.changes(100, 101);
+    }
+
+    @Test
+    public void whenNotEnoughChangesThenReturnValue() {
+        CoffeeMachine cf = new CoffeeMachine(10, 0, 0, 0);
+        int[] result = cf.changes(100, 82);
+        int[] expected = new int[] {100};
+        assertThat(result, is(expected));
     }
 }
