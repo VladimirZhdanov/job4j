8f037b42aef4e2c7a08135a94503d341b0e7e9cb
VladimirZhdanov <consthomespb@gmail.com>
2019-03-20 08:10:18 +0300

    1. Проверить байтовый поток 3

--- chapter_006/src/main/java/ru/job4j/io/CheckNumber.java
+++ chapter_006/src/main/java/ru/job4j/io/CheckNumber.java
@@ -1,10 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.math.BigInteger;
+import java.io.*;
 
 /**
  * CheckNumber
@@ -16,12 +12,19 @@ import java.math.BigInteger;
 public class CheckNumber {
     boolean isNumber(InputStream in) {
         var result = false;
+        ByteArrayInputStream bais = null;
         BufferedReader br = null;
         try {
             br = new BufferedReader(new InputStreamReader(in));
-            String line =  br.readLine();
+            String line = br.readLine();
+            byte[] lineOfBytes =  in.readAllBytes();
+            for (byte value : lineOfBytes) {
+                if (value < 48 || value > 57) {
+                    throw new NumberFormatException();
+                }
+                //System.out.println(value);
+            }
             int lastSymbol = line.charAt(line.length() - 1);
-            BigInteger justNumberOfInputStream = new BigInteger(line);
             if (lastSymbol % 2 == 0) {
                 result = true;
             }
