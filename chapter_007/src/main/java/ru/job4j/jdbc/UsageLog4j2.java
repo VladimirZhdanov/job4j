9ff35f429a4cc88f9fbcfe70b27e21ec0776f739
VladimirZhdanov <consthomespb@gmail.com>
2019-04-21 12:41:08 +0300

    4.1. Log4j 2. Логирование системы.

--- chapter_007/src/main/java/ru/job4j/jdbc/UsageLog4j2.java
+++ chapter_007/src/main/java/ru/job4j/jdbc/UsageLog4j2.java
@@ -8,6 +8,7 @@ public class UsageLog4j2 {
     private static final Logger LOG = LogManager.getLogger(UsageLog4j2.class.getName());
 
     public static void main(String[] args) {
+        //System.out.println("wer");
         LOG.trace("trace message");
         LOG.debug("debug message");
         LOG.info("info message");
--- chapter_007_SQL_JDBC/src/main/java/ru/job4j/jdbc/Test.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.jdbc;
-
-public class Test {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Class.forName("org.postgresql.Driver");
-    }
-}
