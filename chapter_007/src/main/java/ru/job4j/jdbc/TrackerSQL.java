cae4156e61c03615bf5d367bc55cd92c96e702a3
VladimirZhdanov <consthomespb@gmail.com>
2019-04-26 20:11:38 +0300

    TrackerSQL

--- chapter_007/src/main/java/ru/job4j/jdbc/TrackerSQL.java
+++ chapter_007/src/main/java/ru/job4j/jdbc/TrackerSQL.java
@@ -1,17 +1,38 @@
 package ru.job4j.jdbc;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.job4j.tracker.ITracker;
 import ru.job4j.tracker.Item;
 
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
 
 public class TrackerSQL implements ITracker, AutoCloseable {
+    private static final Logger LOGGER = LogManager.getLogger(TrackerSQL.class.getName());
 
     private Connection connection;
+
+    public TrackerSQL() {
+        this.init();
+    }
+
+    public void setTableItems() {
+        try (PreparedStatement prepStatement =
+                     this.connection.prepareStatement(
+                             "create table if not exists items (id serial primary key, item_name varchar(250)," +
+                                     " description text, created timestamp, comments text);")
+        ) {
+            prepStatement.executeUpdate();
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage(), e);
+        }
+    }
 
     public boolean init() {
         try (InputStream in = TrackerSQL.class.getClassLoader().getResourceAsStream("app.properties")) {
